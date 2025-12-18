00c6448f643870edf9577f958315f74154100e57
Kerstin Andersson <gast@kvinnojourenanna.se>
2025-12-18 11:41:02 +0100

    first commit

---
+++ app/addItem/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AddItem() {
+  return (
+    <div>
+      <h2>Lägg till +</h2>
+    </div>
+  )
+}
---
+++ app/components/Navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+
+export default function Navbar() {
+  return (
+    <div>
+        <ul>
+            <li><a href="/addItem">Add Item</a></li>
+        </ul>
+    </div>
+  )
+}
